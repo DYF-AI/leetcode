45f32c27b487e9ddfc8d08cf0f3254e445c4af72
DYF-AI <54470509+DYF-AI@users.noreply.github.com>
2024-04-06 17:28:10 +0800

    first-commit

---
+++ editor/cn/[1483]树节点的第 K 个祖先.py
@@ -0,0 +1,24 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class TreeAncestor(object):
+
+    def __init__(self, n, parent):
+        """
+        :type n: int
+        :type parent: List[int]
+        """
+
+
+    def getKthAncestor(self, node, k):
+        """
+        :type node: int
+        :type k: int
+        :rtype: int
+        """
+
+
+
+# Your TreeAncestor object will be instantiated and called as such:
+# obj = TreeAncestor(n, parent)
+# param_1 = obj.getKthAncestor(node,k)
+# leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[18]四数之和.py
@@ -0,0 +1,10 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+# leetcode submit region end(Prohibit modification and deletion)
